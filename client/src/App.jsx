751a94663dc901aac3bf838e05a5710e560c75fa
xmeix <lamiaboualouache@gmail.com>
2023-08-12 13:00:30 +0100

    adding-test-navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <LoginPage />
     </div>
   );
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return <div className="navbar">nav</div>;
+};
+
+export default Navbar;
